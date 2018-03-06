fc4875c0466cb741d3bdf10dfde33dea04df9bf8
codeserra <36965166+codeserra@users.noreply.github.com>
2018-03-06 23:42:52 +0530

    Update LevelOrderTraversal.cpp

--- LevelOrderTraversal.cpp
+++ LevelOrderTraversal.cpp
@@ -1,3 +1,14 @@
+// level order traversal is defined as follows
+// visit root
+// go to next level and visit all nodes at that level
+// repeat until all levels are complete
+// for a below tree level order traversal will be 1, 2, 3, 4, 5, 6, 7
+//        1        
+//      /   \                   
+//     2      3
+//    / \    / \
+//   4   5  6   7
+
 void  LevelOrder ( struct BinaryTreeNode* root
 {
       struct BinaryTreeNode* temp;
