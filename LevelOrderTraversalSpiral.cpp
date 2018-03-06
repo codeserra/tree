33c6c2fa6535f2b2feeaa05dd98f0fa3d5bc2ff4
codeserra <36965166+codeserra@users.noreply.github.com>
2018-03-06 10:38:34 +0530

    Create LevelOrderTraversalSpiral.cpp

---
+++ LevelOrderTraversalSpiral.cpp
@@ -0,0 +1,10 @@
+//   Objective:
+//                 1
+//               /   \
+//              2     3
+//            /  \   /  \
+//           4   5  6    7 
+//  Write a function to print tree in zig-zag format, i.e above tree print the output as: 1, 2, 3, 7, 6, 5, 4
+//
+// Hint: use a flag to know which direction to be filled, if direction is left to right use queue else use stack.
+
